b334378b1e475990f9f663c6d195e1e30d965f87
yancytong <tcy2002tcy@gmail.com>
2026-03-26 00:34:05 +0800

    add some comments

--- newton/_src/solvers/ca_exercises/solver_exercise2_constraint_pendulum.py
+++ newton/_src/solvers/ca_exercises/solver_exercise2_constraint_pendulum.py
@@ -79,53 +79,6 @@ def pendulum_kernel(
     #   4. You can use the classes and functions defined above, or implement your own 
     #      linear algebra utilities if you prefer
 
-    x0 = body_q_in[0]
-    v0 = body_qd_in[0]
-    p0 = x0.p
-    lin_v0 = wp.vec3(v0[0], v0[1], v0[2])
-
-    x1 = body_q_in[1]
-    v1 = body_qd_in[1]
-    p1 = x1.p
-    lin_v1 = wp.vec3(v1[0], v1[1], v1[2])
-
-    J = mat26(
-        p0[0], p0[1], p0[2], 0.0, 0.0, 0.0,
-        p0[0] - p1[0], p0[1] - p1[1], p0[2] - p1[2], p1[0] - p0[0], p1[1] - p0[1], p1[2] - p0[2],
-    )
-    J1 = mat26(
-        v0[0], v0[1], v0[2], 0.0, 0.0, 0.0,
-        v0[0] - v1[0], v0[1] - v1[1], v0[2] - v1[2], v1[0] - v0[0], v1[1] - v0[1], v1[2] - v0[2],
-    )
-    W = mat66(
-        1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
-        0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
-        0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
-        0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
-        0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
-    )
-    q1 = vec6(lin_v0[0], lin_v0[1], lin_v0[2], lin_v1[0], lin_v1[1], lin_v1[2])
-    f = vec6(gravity[0], gravity[1], gravity[2], gravity[0], gravity[1], gravity[2])
-
-    A = J @ W @ wp.transpose(J)
-    b = -J1 @ q1 - J @ W @ f
-
-    lambda_ = ldlt(A, b)
-    F = wp.transpose(J) @ lambda_ + f
-    a1 = F[0:3]
-    a2 = F[3:6]
-
-    lin_v0_new = lin_v0 + a1 * dt
-    lin_v1_new = lin_v1 + a2 * dt
-    p0_new = p0 + lin_v0_new * dt
-    p1_new = p1 + lin_v1_new * dt
-
-    body_q_out[0] = wp.transform(p=p0_new, q=x0.q)
-    body_q_out[1] = wp.transform(p=p1_new, q=x1.q)
-    body_qd_out[0] = wp.spatial_vector(lin_v0_new, wp.vec3(v0[3], v0[4], v0[5]))
-    body_qd_out[1] = wp.spatial_vector(lin_v1_new, wp.vec3(v1[3], v1[4], v1[5]))
-
 
 class SolverExercise2ConstraintPendulum(SolverBase):
     def __init__(self, model: Model):
